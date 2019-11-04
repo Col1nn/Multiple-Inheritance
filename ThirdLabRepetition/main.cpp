8273cfd5d301534c78c0f01f91fcda9c94e0dab0
Col1nn <pepito98@tlen.pl>
2019-11-04 19:35:33 +0100

    Multiple Inheritance

--- ThirdLabRepetition/main.cpp
+++ ThirdLabRepetition/main.cpp
@@ -6,10 +6,64 @@
 //  Copyright © 2019 Mateusz Kot. All rights reserved.
 //
 
-#include <iostream>
+/*
+Make simple 4 classes with default constructors showing a message -> B and C
+inherit from A, D inherits from B and C later provide method m in A, call d.m
+in main later provide virtual
+*/
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+#include <iostream>
+using namespace std;
+
+class A
+{
+public:
+    A()
+    {
+        cout << "Class A constructor" << endl;
+    }
+    void m()
+    {
+        cout << "Multiple inheritance" << endl;
+    }
+    
+};
+
+class B: public virtual A
+{
+public:
+    B()
+    {
+        cout << "Class B(constructor) inherits from class A" << endl;
+        
+    }
+    
+};
+
+class C: public virtual A
+{
+public:
+    C()
+    {
+        cout << "Class C(constructor) inherits from class A" << endl;
+    }
+};
+
+class D: public B, public C
+{
+public:
+    D()
+    {
+        cout << "Class D(constructor) inherits from class A and B" << endl;
+
+    }
+    
+};
+
+int main()
+{
+    D d;
+    d.m();
     return 0;
 }
+
