d1364c3e72c62e6c5491b090c6cd8da6c2c188c7
Col1nn <pepito98@tlen.pl>
2019-11-04 19:30:57 +0100

    Initial Commit

---
+++ ThirdLabRepetition/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ThirdLabRepetition
+//
+//  Created by Mateusz Kot on 04/11/2019.
+//  Copyright © 2019 Mateusz Kot. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
